25b29e519ebf0b364cf15e4b6ddbb1f70ff524f2
Yared <yayeyaye6@gmail.com>
2020-02-13 04:09:03 +0100

    logic code

--- scripts/logic.js
+++ scripts/logic.js
@@ -35,7 +35,24 @@ function doMath(operation, a, b) {
   }
   // write your code below this comment:
 
-
+  unction calc() {
+    var a = parseInt(document.querySelector("#value1").value);
+    var b = parseInt(document.querySelector("#value2").value);
+    var op = document.querySelector("#operator").value;
+    var calculate;
+    
+    if (op == "add") { 
+        calculate = a + b;
+    } else if (op =="min"){
+        calculate = a - b;   
+    } else if (op =="div"){
+        calculate = a / b;   
+    } else if (op =="mul"){
+        calculate = a * b;   
+    }
+    document.querySelector("#result").innerHTML = calculate; 
+    }   
+    
 
 }
 
